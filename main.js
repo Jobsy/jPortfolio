6962faa864b10c6069f4440fe8533f4f74365377
Oluwajoba <oluwajobabello@gmail.com>
2020-05-25 23:58:37 +0100

    add functionality to overlay for hamburger mobile icon / index.html

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
 const hLinks = document.getElementById("hLinks");
+const menuAnchor1 = document.getElementById("menu-anchor1");
+const menuAnchor2 = document.getElementById("menu-anchor2");
+const menuAnchor3 = document.getElementById("menu-anchor3");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
@@ -12,6 +15,31 @@ menuBtn.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+menuAnchor1.addEventListener("click", () => {
+  menuBtn.classList.remove("open");
+  hLinks.classList.remove("open");
+  menuOpen = false;
+});
+menuAnchor2.addEventListener("click", () => {
+  menuBtn.classList.remove("open");
+  hLinks.classList.remove("open");
+  menuOpen = false;
+});
+menuAnchor3.addEventListener("click", () => {
+  menuBtn.classList.remove("open");
+  hLinks.classList.remove("open");
+  menuOpen = false;
+});
+// body.addEventListener("click", (event) => {
+//   if (event.target !== menuAnchor[0] || event.target !== menuAnchor[1]) {
+//     return;
+//   } else {
+//     menuBtn.classList.remove("open");
+//     hLinks.classList.remove("open");
+//     menuOpen = false;
+//   }
+// });
 
 function toggleBtn1(x) {
   var element = document.body;
